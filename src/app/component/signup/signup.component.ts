cdf3934faea9f59d41f9548dbfe0c6b77048012a
axokda <axokda@gmil.com>
2024-08-25 16:25:30 +0300

    Repassword update

--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AlertErrorComponent } from "../../shared/alert-error/alert-error.component";
+import { confirmPassword } from '../../shared/utils/confirm-password.utils';
+import { signupValidators } from '../../shared/Validators/register.Validators';
+
+
+
 
 @Component({
   selector: 'app-signup',
@@ -13,17 +18,18 @@ export class SignupComponent {
 
 
   register = new FormGroup({
-    name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
-    email: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null,[Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')] ),
-    rePassword: new FormControl(null, [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]),
-  })
+    name: new FormControl(null,signupValidators.name),
+    email: new FormControl(null,signupValidators.email),
+    password: new FormControl(null,signupValidators.password),
+    rePassword: new FormControl(null),
+  }, confirmPassword)
+
 
   sendData() {
-if (this.register.valid) {
+    if (this.register.valid) {
       console.log(this.register.value);
 
-}
+    }
 
   }
 
---
+++ src/app/shared/Validators/register.Validators.ts
@@ -0,0 +1,9 @@
+import { Validators } from "@angular/forms";
+
+
+
+export const signupValidators = {
+    name:  [Validators.required, Validators.minLength(3), Validators.maxLength(30)],
+    email:  [Validators.required, Validators.email],
+    password:  [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')],
+}
